8a97fe3191eaf8abb7dabd91308c421042c1aefa
Michael Frain <michael.frain@hudl.com>
2017-08-22 15:49:10 -0400

    More detail to the table view

--- public/javascripts/foulsearch.js
+++ public/javascripts/foulsearch.js
@@ -13,8 +13,20 @@ $(document).ready(function() {
             url: "/api/fouls?user="+user
         }).then(function(data) {
             if (data) {
+              var team = "";
+              if (data[i].homeTeam == true) {
+                team = "H";
+              } else {
+                team = "V";
+              }
+              var grade = "";
+              if (data[i].grade == null) {
+                grade = "";
+              } else {
+                grade = data[i].grade;
+              }
               for (var i=0; i<data.length; i++) {
-                $('#foulelement').append('<tr><td>'+data[i].game.home+' vs. '+data[i].game.visitor+'</td><td>'+data[i].quarter+'</td><td>'+data[i].time+'</td><td>'+data[i].foul+'</td></tr>');
+                $('#foulelement').append('<tr><td>'+data[i].game.home+' vs. '+data[i].game.visitor+'</td><td>'+data[i].quarter+'</td><td>'+data[i].time+'</td><td>'+team+'</td><td>'+data[i].foul+'</td><td>'+data[i].odrk+'</td><td>'+data[i].player+'</td><td>'+data[i].ado+'</td><td>'+data[i].officials+'</td><td>'+data[i].comment+'</td><td>'+data[i].evaluatorComment+'</td><td>'+data[i].supervisorComment+'</td><td>'+grade+'</td></tr>');
               }
               $('table').css("display", "inline");
             }
