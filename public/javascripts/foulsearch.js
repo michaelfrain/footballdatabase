46ccbe1afcd15d60dd7b76e4e52c9f9b20811e1f
Michael Frain <michael.frain@hudl.com>
2017-08-22 16:12:30 -0400

    Modified add user submission button behavior

--- public/javascripts/foulsearch.js
+++ public/javascripts/foulsearch.js
@@ -13,20 +13,40 @@ $(document).ready(function() {
             url: "/api/fouls?user="+user
         }).then(function(data) {
             if (data) {
-              var team = "";
-              if (data[i].homeTeam == true) {
-                team = "H";
-              } else {
-                team = "V";
-              }
-              var grade = "";
-              if (data[i].grade == null) {
-                grade = "";
-              } else {
-                grade = data[i].grade;
-              }
-              for (var i=0; i<data.length; i++) {
-                $('#foulelement').append('<tr><td>'+data[i].game.home+' vs. '+data[i].game.visitor+'</td><td>'+data[i].quarter+'</td><td>'+data[i].time+'</td><td>'+team+'</td><td>'+data[i].foul+'</td><td>'+data[i].odrk+'</td><td>'+data[i].player+'</td><td>'+data[i].ado+'</td><td>'+data[i].officials+'</td><td>'+data[i].comment+'</td><td>'+data[i].evaluatorComment+'</td><td>'+data[i].supervisorComment+'</td><td>'+grade+'</td></tr>');
+                for (var i=0; i<data.length; i++) {
+                  var team = "";
+                if (data[i].homeTeam == undefined) {
+                  team = "";
+                } else if (data[i].homeTeam == true) {
+                  team = "H";
+                } else {
+                  team = "V";
+                }
+                var grade = "";
+                if (data[i].grade == undefined) {
+                  grade = "";
+                } else {
+                  grade = data[i].grade;
+                }
+                var odrk = "";
+                if (data[i].odrk == 0) {
+                  odrk = "O";
+                } else if (data[i].odrk == 1) {
+                  odrk = "D";
+                } else if (data[i].odrk == 2) {
+                  odrk = "R";
+                } else if (data[i].odrk == 3) {
+                  odrk = "K";
+                }
+                var ado = "";
+                if (data[i].ado == 0) {
+                  adk = "A";
+                } else if (data[i].ado == 1) {
+                  ado = "D";
+                } else if (data[i].ado == 2) {
+                  ado = "O";
+                }
+                $('#foulelement').append('<tr><td>'+data[i].game.home+' vs. '+data[i].game.visitor+'</td><td>'+data[i].quarter+'</td><td>'+data[i].time+'</td><td>'+team+'</td><td>'+data[i].foul+'</td><td>'+odrk+'</td><td>'+data[i].player+'</td><td>'+ado+'</td><td>'+data[i].officials+'</td><td>'+data[i].comment+'</td><td>'+data[i].evaluatorComment+'</td><td>'+data[i].supervisorComment+'</td><td>'+grade+'</td></tr>');
               }
               $('table').css("display", "inline");
             }
---
+++ public/javascripts/userinput.js
@@ -0,0 +1,12 @@
+$(document).ready(function() {
+    $('#postuser').click(function() {
+        var data = $('#newuser').serializeJSON();
+        var userString = JSON.stringify(data);
+        $.ajax({
+            url: "/api/users",
+            type: "POST",
+            contentType: "application/json",
+            data: userString
+        });
+    });
+});
