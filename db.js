d500490f75fc702803aac9d8fb19d41313efc4ae
Michael Frain <michael.frain@hudl.com>
2017-10-24 18:02:28 -0400

    Fixed database name

--- db.js
+++ db.js
@@ -1,3 +1,3 @@
 module.exports = {
-    'url' : 'mongodb://michaelfrain:mapleleaf1@sciaccluster0-shard-00-00-luq5f.mongodb.net:27017,sciaccluster0-shard-00-01-luq5f.mongodb.net:27017,sciaccluster0-shard-00-02-luq5f.mongodb.net:27017/test?ssl=true&replicaSet=SCIACCluster0-shard-0&authSource=admin'
+    'url' : 'mongodb://michaelfrain:mapleleaf1@sciaccluster0-shard-00-00-luq5f.mongodb.net:27017,sciaccluster0-shard-00-01-luq5f.mongodb.net:27017,sciaccluster0-shard-00-02-luq5f.mongodb.net:27017/fouldatabase?ssl=true&replicaSet=SCIACCluster0-shard-0&authSource=admin'
 }
